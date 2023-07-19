33c32f14b5fbdf4c397c0a58f2819f2d6f6fe228
Jedy Matt Tabasco <jedymatt@gmail.com>
2023-07-19 21:58:46 +0800

    Update price_result_screen.dart

--- lib/price_result_screen.dart
+++ lib/price_result_screen.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
 
 class PriceResultScreen extends StatelessWidget {
-  const PriceResultScreen({super.key});
+  const PriceResultScreen({super.key, required this.barcodeValue});
+  final String barcodeValue;
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Column(
+        children: [
+          Text(barcodeValue),
+        ],
+      ),
+    );
   }
 }
